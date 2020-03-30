7ff9c2419200579ffc1fbbda7d917fb6829bd267
Nicolò Battaglia <gaara0633@gmail.com>
2020-03-30 18:33:07 +0200

    Parole palindrome fine

--- palindrome/home.js
+++ palindrome/home.js
@@ -1,1 +1,10 @@
-var parolaUtente = prompt("Inserisci una parola")
+var parola = prompt("Inserisci una parola")
+function check_palindroma(){
+  if(parola.split("").reverse().join("") === parola){
+    var msg = "Parola Palindroma"
+  }else{
+    var msg = "Parola Non Palindroma"
+  }
+  document.getElementById('mio_id').innerHTML = msg;
+}
+check_palindroma(parola)
